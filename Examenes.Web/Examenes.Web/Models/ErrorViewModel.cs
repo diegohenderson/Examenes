3a5104f881267502c161fd2770ff424a90ddb353
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-11-16 14:56:46 -0300

    Solucion inicial + data/entities

---
+++ Examenes.Web/Examenes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Examenes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
